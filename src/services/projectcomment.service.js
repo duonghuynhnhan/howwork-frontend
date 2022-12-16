775b51dbf00b741114c66c9a1bf852aa07c9bdf7
duonghuynhnhan <dhuynhnhan@gmail.com>
2022-12-16 11:13:18 +0700

    finished

--- src/services/projectcomment.service.js
+++ src/services/projectcomment.service.js
@@ -23,34 +23,6 @@ class ProjectCommentService {
   async delete(comment_id) {
     return (await this.api.delete(`${url}/api/project/comment/${comment_id}`)).data
   }
-
-  //   async detail(project_id) {
-  //     return (await this.api.get(`${url}/api/project/${project_id}`)).data
-  //   }
-
-  //   async getMany() {
-  //     return (await this.api.get(this.baseUrl)).data
-  //   }
-
-  //   async create(payload) {
-  //     return (await this.api.post(`${url}/api/project`, payload)).data
-  //   }
-
-  //   async deleteMany() {
-  //     return (await this.api.delete(this.baseUrl)).data
-  //   }
-
-  //   async get(id) {
-  //     return (await this.api.get(`${this.baseUrl}/${id}`)).data
-  //   }
-
-  //   async update(id, admin) {
-  //     return (await this.api.put(`${this.baseUrl}/${id}`, admin)).data
-  //   }
-
-  //   async delete(id) {
-  //     return (await this.api.delete(`${this.baseUrl}/${id}`)).data
-  //   }
 }
 
 export const projectCommentService = new ProjectCommentService()
